2755b069cfc171e986a963caf72db6e21f14c244
polmes <pol.developer1@gmail.com>
2016-11-12 00:31:29 +0100

    fs watch test

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-  var viewer = new Cesium.Viewer('cesiumContainer');
+var viewer = new Cesium.Viewer('cesiumContainer');
+
+var fs = require('fs');
+fs.watch('test.csv', function() {
+	console.log("File changed");
+});
