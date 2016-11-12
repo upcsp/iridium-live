9042729ddcfc1f0e98c86624fe714474a11d78ee
polmes <pol.developer1@gmail.com>
2016-11-12 01:12:34 +0100

    added ws

--- back.js
+++ back.js
@@ -1,6 +1,14 @@
 var fs = require('fs');
+
+var WebSocket = require('ws');
+var ws = new WebSocket('ws://localhost:3030');
+
 fs.watch('test.csv', function(eventType, filename) {
-	if (eventType == 'change')	{
+	if (eventType == 'change') {
 		console.log("File changed");
+
+		ws.on('open', function() {
+			ws.send('hello');
+		});
 	}
 });
