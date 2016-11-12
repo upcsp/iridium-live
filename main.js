48303a0451ca222de7e3a2f2d978590d322221ea
bobz <you@example.com>
2016-11-12 01:07:29 +0100

    WebSocket Client

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
+
+var connection = new WebSocket('ws://localhost:3030');
+
+connection.onmessage = function (evt) { 
+  var received_msg = evt.data;
+  alert("Message is received...");
+  console.log(evt.data):
+};
+		
+
+var position = Cesium.Cartesian3.fromDegrees(-123.0744619, 44.0503706, 0);
+var heading = Cesium.Math.toRadians(45.0);
+var pitch = Cesium.Math.toRadians(15.0);
+var roll = Cesium.Math.toRadians(0.0);
+var orientation = Cesium.Transforms.headingPitchRollQuaternion(position, heading, pitch, roll);
+
+var entity = viewer.entities.add({
+    position : position,
+	orientation : orientation,
+    model : {
+        uri : './assets/balloon_finalv4.glb'
+    }
+});
+viewer.trackedEntity = entity;
