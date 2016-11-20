7016f1610da52bf1bc5b754e750c52db6ea37297
polmes <pol.developer1@gmail.com>
2016-11-20 20:02:59 +0100

    connect to MySQL DB

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+
+$config = parse_ini_file( 'config.ini' );
+if ( ! $config ) die( "Config access error" );
+
+try {
+	$connection = new PDO( 'mysql:host=' . $config['server'] . ';dbname=' . $config['database'],  $config['user'], $config['password'] );
+} catch ( PDOException $e ) {
+	die( "DB connection error: " . $e->getMessage() );
+}
