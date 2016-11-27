5b84855738c559a7bc91276bd2dd127bb97501a7
polmes <pol.developer1@gmail.com>
2016-11-27 18:24:11 +0100

    updated event names and implemented 'onopen' functionality

--- main.js
+++ main.js
@@ -4,15 +4,26 @@ if ( typeof( EventSource ) !== 'undefined' ) {
 	var stream = new EventSource( 'stream.php' );
 
 	stream.onopen = function( e ) {
-		// Remove previous data points (if any) and add all the data
+		// Remove previous data points (if any)
 	};
 
+	stream.addEventListener( 'start', function( e ) {
+		// Add all previous data points
+		console.log("START");
+		var data = JSON.parse( e.data );
+		console.log( data );
+	});
+
 	stream.addEventListener( 'update', function( e ) {
-		// We'll update position only once we receive an "update" event
+		// We will update position only when we receive an "update" event
+		console.log("UPDATE");
+		var data = JSON.parse( e.data );
+		console.log( data );
 	});
 
 	stream.onmessage = function( e ) { 
 		// onmessage is a generic event, only to be used for testing purposes
+		console.log("ONMESSAGE");
 		var data = JSON.parse( e.data );
 		console.log( data );
 	};
--- stream.php
+++ stream.php
@@ -5,20 +5,34 @@ header( 'Cache-Control: no-cache' );
 
 require_once 'connect.php';
 
-// Send all previous data points for the "onopen" event
-
 // We'll be using PHP_EOL instead of \n for multi-arch compatibility
 
-$id = 0; // should be last sent when fullyimplemented
+// When a new client connects, get all the previous data points
+$query = "SELECT * FROM final_test";
+$statement = $connection->query( $query );
+$rows = $statement->fetchAll( PDO::FETCH_ASSOC );
+
+foreach( $rows as $row ) {
+	// Send all previous data points as the "start" event
+	echo 'id: ' . $row['mysql_id'] . PHP_EOL;
+	echo 'event: start' . PHP_EOL;
+	echo 'data: { "id": "' . $row['mysql_id'] . '", "time": "' . $row['transmit_time'] . '", "latitude": "' . $row['GPS_lat'] . '", "longitude": "' . $row['GPS_long'] . '", "altitude": "' . $row['GPS_h'] . '" }' . PHP_EOL;
+	echo PHP_EOL; // \n\n necessary for Server-Sent Events
+
+	ob_flush();
+	flush();
+}
+
+$id = count( $rows ); // last message sent
 
 while ( true ) {
 	// Check if we have a new data point
-
 	$query = "SELECT MAX( mysql_id ) FROM final_test";
 	$statement = $connection->query( $query );
 	$row = $statement->fetch( PDO::FETCH_ASSOC );
 
 	if ( $row['MAX( mysql_id )'] > $id ) {
+		// Send "update" event when we have new data
 		$query = "SELECT * FROM final_test WHERE mysql_id = :mysql_id";
 		$statement = $connection->prepare( $query );
 
@@ -28,7 +42,7 @@ while ( true ) {
 		$row = $statement->fetch( PDO::FETCH_ASSOC );
 
 		echo 'id: ' . $row['mysql_id'] . PHP_EOL;
-		// echo 'event: update' . PHP_EOL;
+		echo 'event: update' . PHP_EOL;
 		echo 'data: { "id": "' . $row['mysql_id'] . '", "time": "' . $row['transmit_time'] . '", "latitude": "' . $row['GPS_lat'] . '", "longitude": "' . $row['GPS_long'] . '", "altitude": "' . $row['GPS_h'] . '" }' . PHP_EOL;
 		echo PHP_EOL; // \n\n necessary for Server-Sent Events
 
