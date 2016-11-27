032bafccad273301b9c85ac3e4f40a98e38b76f6
polmes <pol.developer1@gmail.com>
2016-11-27 20:05:20 +0100

    check if table from reference.php exists; if it doesn't, create it

--- post2db.php
+++ post2db.php
@@ -1,7 +1,8 @@
 <?php
 
-// Will get data from Iridium $_POST (will need to check origin). This is just a test.
+// Will get data from Iridium $_POST (should check origin)
 
+require 'reference.php';
 require_once 'connect.php';
 
 foreach ($_POST as $key => $value) {
@@ -9,7 +10,21 @@ foreach ($_POST as $key => $value) {
 	echo "$key is $value" . PHP_EOL;
 }
 
-$query = "INSERT INTO final_test ( transmit_time, GPS_lat, GPS_long, GPS_h ) VALUES ( :transmit_time, :GPS_lat, :GPS_long, :GPS_h )";
+// Will trust the user with no prepared statement
+$query = "SHOW TABLES LIKE '" . $reference . "'";
+$statement = $connection->query( $query );
+
+// Check if table exists
+if ( $statement->rowCount() == 0 ) {
+	try {
+		$query = "CREATE TABLE IF NOT EXISTS `" . $reference . "` ( `mysql_id` int( 11 ) unsigned NOT NULL AUTO_INCREMENT, `transmit_time` varchar( 17 ) DEFAULT NULL, `GPS_lat` decimal( 7,4 ) DEFAULT NULL, `GPS_long` decimal( 7,4 ) DEFAULT NULL, `GPS_h` int( 11 ) DEFAULT NULL, PRIMARY KEY ( `mysql_id` ) ) ENGINE=InnoDB DEFAULT CHARSET=utf8";
+		$connection->exec( $query );
+	} catch ( PDOException $e ) {
+		die( "DB table creation error: " . $e->getMessage() );
+	}
+}
+
+$query = "INSERT INTO " . $reference . " ( transmit_time, GPS_lat, GPS_long, GPS_h ) VALUES ( :transmit_time, :GPS_lat, :GPS_long, :GPS_h )";
 $statement = $connection->prepare( $query );
 
 // $statement->bindParam( ':mysql_id', $_POST['mysql_id'] );
